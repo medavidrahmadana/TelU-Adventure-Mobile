40bc999f47d049756a7cd43f796c4fcc0c731656
Iqius <iniiqius@gmail.com>
2024-05-22 05:43:48 +0700

    notification local masih ngebug

--- telu_adventure/lib/widget/modal_barang.dart
+++ telu_adventure/lib/widget/modal_barang.dart
@@ -2,24 +2,84 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:telu_adventure/controllers/lapor_controller.dart';
 import 'package:telu_adventure/model/barang_model.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-class modal_barang extends StatelessWidget {
-  final String documentId; // Mengasumsikan Anda meneruskan ID dokumen ke modal
+class modal_barang extends StatefulWidget {
+  final String documentId;
+
+  modal_barang({Key? key, required this.documentId}) : super(key: key);
+
+  @override
+  _modal_barangState createState() => _modal_barangState();
+}
+
+class _modal_barangState extends State<modal_barang> {
   final TextEditingController _namaBarangController = TextEditingController();
   final TextEditingController _deskripsiBarangController =
       TextEditingController();
   final List<String> _statusOptions = ['Sudah', 'Belum'];
-  String _selectedStatus = 'Belum'; // Nilai default
+  String _selectedStatus = 'Belum';
 
-  modal_barang({Key? key, required this.documentId}) : super(key: key);
+  static final FlutterLocalNotificationsPlugin _flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  @override
+  void initState() {
+    super.initState();
+    initializeNotifications();
+  }
+
+  Future<void> initializeNotifications() async {
+    const AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings('logo');
+
+    const InitializationSettings initializationSettings =
+        InitializationSettings(android: initializationSettingsAndroid);
+
+    const AndroidNotificationChannel channel = AndroidNotificationChannel(
+      'Test Notif', // Ganti dengan ID channel Anda
+      'Barang Ditemukan', // Ganti dengan nama channel Anda
+      description: 'notif ketika status barang sudah ditemukan',
+      importance: Importance.high,
+    );
+
+    await _flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+            AndroidFlutterLocalNotificationsPlugin>()
+        ?.createNotificationChannel(channel);
+
+    await _flutterLocalNotificationsPlugin.initialize(initializationSettings);
+  }
+
+  Future<void> showSimpleNotification({
+    required String title,
+    required String body,
+    required String payload,
+  }) async {
+    const AndroidNotificationDetails androidNotificationDetails =
+        AndroidNotificationDetails(
+      'your_channel_id', // Ganti dengan ID channel Anda
+      'your_channel_name', // Ganti dengan nama channel Anda
+      channelDescription: 'your_channel_description',
+      importance: Importance.max,
+      priority: Priority.high,
+      ticker: 'ticker',
+      icon: 'logo',
+    );
+    const NotificationDetails notificationDetails =
+        NotificationDetails(android: androidNotificationDetails);
+    await _flutterLocalNotificationsPlugin.show(
+        0, title, body, notificationDetails,
+        payload: payload);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Dialog(
       backgroundColor: Colors.white,
-      insetPadding: EdgeInsets.symmetric(horizontal: 25), // Atur inset padding
+      insetPadding: EdgeInsets.symmetric(horizontal: 25),
       child: Container(
-        width: MediaQuery.of(context).size.width * 0.9, // Lebar kontainer utama
+        width: MediaQuery.of(context).size.width * 0.9,
         padding: EdgeInsets.all(20),
         child: Column(
           mainAxisSize: MainAxisSize.min,
@@ -46,14 +106,12 @@ class modal_barang extends StatelessWidget {
                       icon: Icon(Icons.delete, color: Colors.red),
                       onPressed: () {
                         try {
-                          LaporCon.deleteLaporan(
-                              documentId); // Memanggil fungsi deleteKompetisi
-                          Navigator.of(context)
-                              .pop(); // Menutup modal setelah menghapus
+                          LaporCon.deleteLaporan(widget.documentId);
+                          Navigator.of(context).pop();
                           ScaffoldMessenger.of(context).showSnackBar(
                             SnackBar(
                               content: Text('Laporan berhasil dihapus'),
-                              duration: Duration(seconds: 2), // Durasi snackbar
+                              duration: Duration(seconds: 2),
                             ),
                           );
                         } catch (error) {
@@ -67,8 +125,7 @@ class modal_barang extends StatelessWidget {
                         }
                       },
                     ),
-
-                    SizedBox(width: 10), // Untuk keseimbangan
+                    SizedBox(width: 10),
                   ],
                 ),
               ],
@@ -94,7 +151,9 @@ class modal_barang extends StatelessWidget {
               }).toList(),
               onChanged: (newValue) {
                 if (newValue != null) {
-                  _selectedStatus = newValue;
+                  setState(() {
+                    _selectedStatus = newValue;
+                  });
                 }
               },
               decoration: InputDecoration(
@@ -124,8 +183,7 @@ class modal_barang extends StatelessWidget {
                 ),
                 shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                   RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(
-                        11), // Atur radius sesuai keinginan
+                    borderRadius: BorderRadius.circular(11),
                   ),
                 ),
               ),
@@ -137,17 +195,25 @@ class modal_barang extends StatelessWidget {
                 ),
               ),
               onPressed: () {
+                String selectedStatus = _selectedStatus;
+
+                showSimpleNotification(
+                  title: 'telu adventure',
+                  body: 'Status barang: $selectedStatus',
+                  payload: "This is simple data",
+                );
+
                 Future.delayed(Duration.zero, () {
                   Barang _barang = Barang(
                     nama: _namaBarangController.text,
-                    type: '', // Anda mungkin ingin menanganinya secara berbeda
+                    type: '',
                     deskripsi: _deskripsiBarangController.text,
-                    imagePath: '', // Karena gambar dihapus
-                    telepon: '', // Karena lokasi dihapus
+                    imagePath: '',
+                    telepon: '',
                     kehilangan: FirebaseAuth.instance.currentUser!.uid,
                     status: _selectedStatus,
                   );
-                  LaporCon.updateFirestore(context, documentId, _barang);
+                  LaporCon.updateFirestore(context, widget.documentId, _barang);
                 });
               },
             )
--- telu_adventure/macos/Flutter/GeneratedPluginRegistrant.swift
+++ telu_adventure/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,7 @@ import file_selector_macos
 import firebase_auth
 import firebase_core
 import firebase_storage
+import flutter_local_notifications
 import mobile_scanner
 import sqflite
 import url_launcher_macos
@@ -20,6 +21,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
